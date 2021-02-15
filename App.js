a58ef2476f79d799ee8c1fdea51664826101787d
Basma Mostafa <57597830+basmaamostafa@users.noreply.github.com>
2021-02-15 15:16:16 +0200

    tutorial test

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Explore from "./Component/Explore";
+import { Explore } from "./Component/Explore";
 import Session from "./Component/Session";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import {AdvisorProfile} from "./Component/AdvisorProfile";
+import { AdvisorProfile } from "./Component/AdvisorProfile";
 import Search from "./Component/Search";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { Tutorial1 } from "./Component/Tutorials/Tutorialscreen1";
+import { Tutorial2 } from "./Component/Tutorials/Tutorialscreen2";
+import { Tutorial3 } from "./Component/Tutorials/Tutorialscreen3";
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
-    return <AdvisorProfile />;
+    return <Tutorial3 />;
     return (
       <NavigationContainer>
         <Stack.Navigator>
